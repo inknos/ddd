8543daeb1d5ee8cee8f99a2a54f8d81e3f35eb56
inknos <nicola.sella@outlook.it>
2019-08-13 23:11:16 +0200

    help text

--- database/models.py
+++ database/models.py
@@ -3,7 +3,25 @@ from django.utils import timezone
 
 # Create your models here.
 class Contributor(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(
+        max_length=200,
+        help_text="Name of contributor"
+    )
+
+    email = models.EmailField(
+        max_length=200,
+        default="",
+        blank=True,
+        help_text="Main E-Mail of the contributor"
+    )
+
+    institution = models.ForeignKey(
+        'Institution',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        help_text="Institution where the contributor belongs to",
+    )
 
     date_of_addition = models.DateTimeField(
         auto_now_add=True, editable=False
@@ -16,11 +34,15 @@ class Contributor(models.Model):
         return self.name
 
 class Institution(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(
+        max_length=200,
+        help_text="Institution name"
+    )
 
     date_of_addition = models.DateTimeField(
         auto_now_add=True, editable=False
     )
+
     modification_date = models.DateTimeField(
         auto_now=True, editable=False
     )
@@ -29,11 +51,15 @@ class Institution(models.Model):
         return self.name
 
 class Action(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(
+        max_length=200,
+        help_text="Action name",
+    )
 
     date_of_addition = models.DateTimeField(
         auto_now_add=True, editable=False
     )
+
     modification_date = models.DateTimeField(
         auto_now=True, editable=False
     )
@@ -46,15 +72,16 @@ class Contribution(models.Model):
         on_delete=models.CASCADE,
     )
 
+    action_type = models.ForeignKey(
+        'Action',
+        on_delete=models.CASCADE,
+    )
+
     target_institution = models.ForeignKey(
         'Institution',
         on_delete=models.CASCADE,
     )
 
-    action_type = models.ForeignKey(
-        'Action',
-        on_delete=models.CASCADE,
-    )
     date_of_contribution = models.DateField(default=timezone.now)
     date_of_addition = models.DateTimeField(
         auto_now_add=True, editable=False
@@ -64,3 +91,4 @@ class Contribution(models.Model):
     )
     def __str__(self):
         return self.date_of_contribution.strftime('%m/%d/%Y') + ": " + self.name_of_contributor.name + ": " + self.action_type.name + " " + self.target_institution.name
+
